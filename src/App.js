83edea42dd0d8d3d83643cec66e4ad9c060bd929
JavierLuisG <javierandresluisgonzalez@gmail.com>
2024-03-05 01:29:22 -0500

    Nueva estructura lógica en App.js, eliminación de Navigation.js y creación de todos.json

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
-import Navigation from './components/Navigation';
+import todos from './todos.json';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation titulo="Navigation"/>
-      <img src={logo} className="App-logo" alt="logo" />
-    </div>
-  );
+class App extends Component {
+  // Como react muestra los datos traidos? -> almacenandolos temporalmente en memoria
+  constructor() {
+    super();
+    this.state = {
+      todos: todos.todos
+    }
+  }
+  render() {
+    const todosCard = this.state.todos.map((todo, i) => {
+      return (
+        <div key={i} className='card'>
+          <div className='card-header'>
+            <h3>{todo.title}</h3>
+          </div>          
+          <div className='card-body'>
+            <p>{todo.description}</p>
+          </div>          
+        </div>        
+      )
+    })
+    
+    return (
+      <div className="App">
+        <nav className='navbar navbar-dark bg-dark'>
+          <a href='' className='text-white'>
+            Tareas
+          </a>
+        </nav>         
+        {todosCard}       
+        <img src={logo} className="App-logo" alt="logo" />
+      </div>
+    );
+  }  
 }
 
 export default App;
